fbdc1f75daffd22a33a7b21784c966ab214653f5
riccardo-guido <guidoriccardo02@gmail.com>
2025-09-19 12:18:00 +0200

    snack 2

--- snacks.js
+++ snacks.js
@@ -6,4 +6,13 @@ function getInitials(fullName) {
     .join("");
 }
 
-module.exports = { getInitials };
+function createSlug(text) {
+  return text
+    .toLowerCase() // tutto minuscolo
+    .trim() // rimuove spazi iniziali/finali
+    .replace(/\s+/g, "-") // sostituisce spazi con trattini
+    .replace(/[^\w\-]+/g, "") // rimuove caratteri non alfanumerici
+    .replace(/\-\-+/g, "-"); // evita trattini multipli consecutivi
+}
+
+module.exports = { getInitials, createSlug };
--- snacks.test.js
+++ snacks.test.js
@@ -1,7 +1,17 @@
-const { getInitials } = require("./snacks.js");
+const { getInitials, createSlug } = require("./snacks.js");
+
+//SNACK 1
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
   expect(getInitials("Mario Rossi")).toBe("MR");
   expect(getInitials("Luigi Verdi")).toBe("LV");
   expect(getInitials("Anna Smith")).toBe("AS");
+});
+
+//SNACK 2
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+  expect(createSlug("Hello World")).toBe("hello-world");
+  expect(createSlug("JavaScript is Awesome")).toBe("javascript-is-awesome");
+  expect(createSlug("Unit Testing Rocks")).toBe("unit-testing-rocks");
 });
