7f3ae3068b70a46e47a5a6ae649e8cda7da0d36c
riccardo-guido <guidoriccardo02@gmail.com>
2025-09-19 12:22:04 +0200

    snack 3

--- snacks.js
+++ snacks.js
@@ -15,4 +15,9 @@ function createSlug(text) {
     .replace(/\-\-+/g, "-"); // evita trattini multipli consecutivi
 }
 
-module.exports = { getInitials, createSlug };
+function average(numbers) {
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  return sum / numbers.length;
+}
+
+module.exports = { getInitials, createSlug, average };
--- snacks.test.js
+++ snacks.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug } = require("./snacks.js");
+const { getInitials, createSlug, average } = require("./snacks.js");
 
 //SNACK 1
 
@@ -14,4 +14,12 @@ test("La funzione createSlug restituisce una stringa in lowercase.", () => {
   expect(createSlug("Hello World")).toBe("hello-world");
   expect(createSlug("JavaScript is Awesome")).toBe("javascript-is-awesome");
   expect(createSlug("Unit Testing Rocks")).toBe("unit-testing-rocks");
+});
+
+//SNACK 3
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+  expect(average([1, 2, 3, 4, 5])).toBe(3);
+  expect(average([10, 20, 30])).toBe(20);
+  expect(average([5, 15, 25, 35])).toBe(20);
 });
